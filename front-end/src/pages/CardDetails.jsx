754240151ce296348bcf1141742672ef9aabb2a8
BENTRO <hyn062347@gmail.com>
2025-02-02 17:20:55 +0900

    serverless Functions update

--- front-end/src/pages/CardDetails.jsx
+++ front-end/src/pages/CardDetails.jsx
@@ -1,10 +1,9 @@
 import React, { useEffect, useState } from 'react';
-import { useParams, useNavigate } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import axios from 'axios';
 import Header from '../components/Header';
-import ReactMarkdown from 'react-markdown';
-import './css/CardDetails.css';
 import Markdown from 'react-markdown';
+import './css/CardDetails.css';
 
 function CardDetails() {
   const { cardId } = useParams();
@@ -12,7 +11,9 @@ function CardDetails() {
   const [cardKingdomPrice, setCardKingdomPrice] = useState('Loading...');
   const [cardKingdomFoilPrice, setCardKingdomFoilPrice] = useState('Loading...');
   const [cardSummary, setCardSummary] = useState('Loading...');
-  const API_BASE_URL = 'http://localhost:3001';
+  
+  // Netlify Functions API Base URL 설정
+  const API_BASE_URL = '/.netlify/functions';
 
   useEffect(() => {
     const fetchCardDetails = async () => {
@@ -34,10 +35,12 @@ function CardDetails() {
           const simplifiedCardName = cardName.includes('//')
             ? cardName.split('//')[0].trim()
             : cardName;
-          const modifiedCollectorNumber = collectorNumber.replace(/\D/g, ''); // \D는 숫자가 아닌 문자 제거
-          const response = await axios.get(`${API_BASE_URL}/api/price`, {
+          const modifiedCollectorNumber = collectorNumber.replace(/\D/g, ''); // 숫자만 남김
+          
+          const response = await axios.get(`${API_BASE_URL}/getCardKingdomPrice`, {
             params: { cardName: simplifiedCardName, collectorNumber: modifiedCollectorNumber },
           });
+
           setCardKingdomPrice(response.data.nonFoilPrice);
           setCardKingdomFoilPrice(response.data.foilPrice);
         } catch (error) {
@@ -52,24 +55,20 @@ function CardDetails() {
   useEffect(() => {
     if (cardDetails) {
       const eventSource = new EventSource(
-        `${API_BASE_URL}/api/summarize-stream?name=${encodeURIComponent(
-          cardDetails.name
-        )}&mana_cost=${encodeURIComponent(cardDetails.mana_cost)}&type_line=${encodeURIComponent(
-          cardDetails.type_line
-        )}&oracle_text=${encodeURIComponent(cardDetails.oracle_text)}`
+        `${API_BASE_URL}/getCardSummary?name=${encodeURIComponent(cardDetails.name)}&mana_cost=${encodeURIComponent(cardDetails.mana_cost)}&type_line=${encodeURIComponent(cardDetails.type_line)}&oracle_text=${encodeURIComponent(cardDetails.oracle_text)}`
       );
 
       console.log(cardDetails.name, cardDetails.type_line, cardDetails.mana_cost, cardDetails.oracle_text);
+
       eventSource.onmessage = (event) => {
         if (event.data === '[DONE]') {
           eventSource.close(); // 스트리밍 종료
         } else {
-          const fromattedData = event.data.replace(/<br>/g, '\n');
-          setCardSummary((prev) => (prev === 'Loading...' ? fromattedData : prev + fromattedData)); // 실시간 데이터 업데이트
-
+          const formattedData = event.data.replace(/<br>/g, '\n');
+          setCardSummary((prev) => (prev === 'Loading...' ? formattedData : prev + formattedData)); // 실시간 데이터 업데이트
         }
       };
-      console.log(cardSummary);
+
       eventSource.onerror = (error) => {
         console.error('Error receiving stream:', error);
         eventSource.close(); // 에러 발생 시 스트리밍 종료
@@ -83,177 +82,33 @@ function CardDetails() {
     return <div>Loading...</div>;
   }
 
-  const renderLegality = (legalities) => {
-    return Object.entries(legalities)
-      .filter(([format, legality]) => legality !== "not_legal") // 필터링
-      .map(([format, legality]) => (
-        <p key={format}>
-          <strong className="capitalize">{format}:</strong> {legality}
-        </p>
-      ));
-  };
-
-  const renderCommonInfo = (cardDetails) => {
-    return (
-      <>
-        <div className='detail_pages_text'>Legalities</div>
-        <p className="legalities">
-          {renderLegality(cardDetails.legalities)}
-        </p>
-        <div className='detail_pages_text'>Other</div>
-        <p className="box-style">
-          <strong>Set Name: </strong>
-          {cardDetails.set_name}
-        </p>
-        <p className="box-style">
-          <Markdown>{cardSummary}</Markdown>
-        </p>
-      </>
-    );
-  }
-
-  const renderCardKingdomPrice = (cardKingdomPrice, cardKingdomFoilPrice, cardDetails) => {
-    const cardName = cardDetails.card_faces ? cardDetails.card_faces[0].name : cardDetails.name;
-    return (
-      <>
-        <a
-          href={`https://www.cardkingdom.com/catalog/search?search=header&filter%5Bname%5D=${encodeURIComponent(cardName)}`}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <p className='Detail_Pages_Card_Number'><strong>CardKingdom: {cardKingdomPrice}</strong></p>
-        </a>
-        <a
-          href={`https://www.cardkingdom.com/catalog/search?filter%5Btab%5D=mtg_foil&filter%5Bsearch%5D=mtg_advanced&filter%5Bname%5D=${encodeURIComponent(cardName)}`}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <p className='Detail_Pages_Card_Number'><strong>CardKingdom(Foil): {cardKingdomFoilPrice}</strong></p>
-        </a>
-      </>
-    );
-  };
-
-  const renderCardImages = (cardDetails) => {
-    if (cardDetails.card_faces) {
-      return (
-        <>
-          <img
-            src={cardDetails.card_faces[0].image_uris.normal}
-            alt="Card Front"
-          />
-          <img
-            src={cardDetails.card_faces[1].image_uris.normal}
-            alt="Card Back"
-          />
-        </>
-      );
-    }
-
-    if (cardDetails.image_uris) {
-      return (
-        <img
-          src={cardDetails.image_uris.normal}
-          alt="Card Front"
-        />
-      );
-    }
-
-    return <p>No images available</p>; // 이미지가 없을 경우 기본 메시지
-  };
-
-  const renderDetail = (cardFace) => {
-    return (
-      <>
-        <p className="box-style"><strong>Name:</strong> {cardFace.name}</p>
-        <p className="box-style"><strong>Mana Cost:</strong> {cardFace.mana_cost}</p>
-        <p className="box-style"><strong>Type:</strong> {cardFace.type_line}</p>
-        <p className="box-style"><strong>Oracle Text</strong><br />{cardFace.oracle_text}</p>
-      </>
-    );
-  }
-
-  const renderArt = (cardDetails) => {
-
-    const artType = cardDetails.textless
-      ? "Textless"
-      : cardDetails.border_color === "borderless"
-        ? "Borderless"
-        : cardDetails.full_art
-          ? "Full Art"
-          : cardDetails.frame_effects?.includes("extendedart")
-            ? "Extended Art"
-            : cardDetails.frame_effects?.includes("showcase")
-              ? "Showcase"
-              : cardDetails.frame_effects?.includes("etched")
-                ? "Etched"
-                : cardDetails.frame === "future"
-                  ? "Future"
-                  : cardDetails.frame === "1997"
-                    ? "Retro"
-                    : "Normal";
-
-    const promoType = cardDetails.promo_types?.includes("bundle")
-      ? "Bundle"
-      : cardDetails.promo_types?.includes("prerelease")
-        ? "Prerelease"
-        : cardDetails.promo_types?.includes("serialized")
-          ? "Serialized"
-          : "Normal";
-    return (
-      <>
-        <p className='Detail_Pages_Card_Number'><strong>Collector Number: {cardDetails.collector_number}</strong></p>
-        <p className="Detail_Pages_Card_Number">
-          <strong>Art Type:</strong> {artType}
-        </p>
-        <p className="Detail_Pages_Card_Number">
-          <strong>Promo Type:</strong> {promoType}
-        </p>
-      </>
-    );
-  }
-
   return (
     <div>
       <Header />
       <div className='Card_Detail_Box'>
-        {cardDetails.card_faces ? (
-          <div className='Flex_Row'>
-            <div className='image-container'>
-              {renderCardImages(cardDetails)}
-              {renderArt(cardDetails)}
-              {renderCardKingdomPrice(cardKingdomPrice, cardKingdomFoilPrice, cardDetails)}
-            </div>
-            <div className='Details_Text'>
-              {/* card details */}
-              <div>
-                {/* front */}
-                <div className='detail_pages_text'>Front</div>
-                {renderDetail(cardDetails.card_faces[0])}
-              </div>
-              <div>
-                {/* back */}
-                <div className='detail_pages_text'>Back</div>
-                {renderDetail(cardDetails.card_faces[1])}
-              </div>
-              {/* Details */}
-              {renderCommonInfo(cardDetails)}
-            </div>
-          </div>
-        ) : (
-          <div className='Flex_Row'>
-            <div className='image-container'>
-              {renderCardImages(cardDetails)}
-              {renderArt(cardDetails)}
-              {renderCardKingdomPrice(cardKingdomPrice, cardKingdomFoilPrice, cardDetails)}
-            </div>
-            <div className='Details_Text'>
-              <div className='detail_pages_text'>Information</div>
-              {renderDetail(cardDetails)}
-              {renderCommonInfo(cardDetails)}
-            </div>
-          </div>
-        )}
+        <div className='image-container'>
+          {cardDetails.image_uris ? (
+            <img src={cardDetails.image_uris.normal} alt="Card Front" />
+          ) : (
+            <p>No images available</p>
+          )}
+          <p className='Detail_Pages_Card_Number'><strong>Collector Number: {cardDetails.collector_number}</strong></p>
+        </div>
+
+        <div className='Details_Text'>
+          <div className='detail_pages_text'>Card Details</div>
+          <p><strong>Name:</strong> {cardDetails.name}</p>
+          <p><strong>Mana Cost:</strong> {cardDetails.mana_cost}</p>
+          <p><strong>Type:</strong> {cardDetails.type_line}</p>
+          <p><strong>Oracle Text:</strong> {cardDetails.oracle_text}</p>
+
+          <div className='detail_pages_text'>Summary</div>
+          <p className="box-style"><Markdown>{cardSummary}</Markdown></p>
+
+          <div className='detail_pages_text'>Prices</div>
+          <p className='Detail_Pages_Card_Number'><strong>CardKingdom: {cardKingdomPrice}</strong></p>
+          <p className='Detail_Pages_Card_Number'><strong>CardKingdom (Foil): {cardKingdomFoilPrice}</strong></p>
+        </div>
       </div>
     </div>
   );
